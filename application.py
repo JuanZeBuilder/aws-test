6f12175cca6bbcdcffbf136264c2ada304ba0250
JuanZeBuilder <92269403+JuanZeBuilder@users.noreply.github.com>
2023-03-18 22:32:53 +0800

    Add files via upload

---
+++ application.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+application = Flask(__name__)
+
+@application.route("/")
+# print a nice greeting.
+def say_hello():
+    return "Hello World!"
+
+
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
