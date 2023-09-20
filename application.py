250b4a997b75bdab74a3f16708631ef09d712e8c
JuanZeBuilder <juanwei1998@gmail.com>
2023-09-20 14:29:34 +0800

    update

--- application.py
+++ application.py
@@ -10,7 +10,4 @@ def say_hello():
 
 # run the app.
 if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
-    application.debug = False
     application.run()
